524ded591e7e4b2abd1c615191365a74bdeadf70
karthik kashyap <karthik553@gmail.com>
2016-09-23 08:21:56 -0400

    Develop (#1)
    * Created workspace with cocoapods
    
    * removed unwanted code.
    
    * added travis.yml
    
    * fixed travis build
    
    * added slack integration
    
    * update travis yml
    
    * travis
    
    * updated travis

---
+++ Myextensions/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Myextensions
+//
+//  Created by Karthik Devulapally on 9/22/16.
+//  Copyright © 2016 Jkdevapps. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+
+# platform :ios, '9.0'
+
+target 'Myextensions' do
+  use_frameworks!
+  pod 'MRCircularProgressView'
+  # Pods for Myextensions
+
+end
